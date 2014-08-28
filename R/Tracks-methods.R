000f0f3dcd7b597fc41239b535fa8be4dd41ff37
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2014-08-28 12:38:32 +0200

    change require() into requireNamespace()

--- R/Tracks-methods.R
+++ R/Tracks-methods.R
@@ -234,7 +234,8 @@ setMethod("plot", "TracksCollection",
 # Provide stcube methods.
 
 map3d = function(map, z, ...) {
-	require(rgl)
+	if (!requireNamespace("rgl", quietly = TRUE))
+		stop("rgl required")
 	if(length(map$tiles) != 1)
 		stop("Pass single map tile only.")
 	nx = map$tiles[[1]]$xres
@@ -247,7 +248,7 @@ map3d = function(map, z, ...) {
 	yc = seq(ymin, ymax, len = nx)
 	col = matrix(data = map$tiles[[1]]$colorData, nrow = ny, ncol = nx)
 	m = matrix(data = z, nrow = ny, ncol = nx)
-	surface3d(x = xc, y = yc, z = m, col = col, ...)
+	rgl::surface3d(x = xc, y = yc, z = m, col = col, ...)
 }
 
 normalize = function(time, by = "week", origin) {
@@ -272,8 +273,10 @@ setMethod("stcube", signature(x = "Track"),
 		ylim, zlim, showMap = FALSE, mapType = "osm", ..., y, z) {
 		# "y" and "z" are ignored, but included in the method signature to avoid
 		# passing them twice to plot3d().
-		require(rgl)
-		require(OpenStreetMap)
+		if (!requireNamespace("rgl", quietly = TRUE))
+			stop("rgl required")
+		if (!requireNamespace("OpenStreetMap", quietly = TRUE))
+			stop("OpenStreetMap required")
 		coords = coordinates(x@sp)
 		time = index(x@time)
 		if(missing(aspect))
@@ -288,15 +291,17 @@ setMethod("stcube", signature(x = "Track"),
 		# rendering everything in one go.
 		if(showMap) {
 			# Required by openmap().
-			require(raster)
-			map = openmap(upperLeft = c(ylim[2], xlim[1]),
+			if (!requireNamespace("raster", quietly = TRUE))
+				stop("raster required")
+			# require(raster)
+			map = OpenStreetMap::openmap(upperLeft = c(ylim[2], xlim[1]),
 				lowerRight = c(ylim[1], xlim[2]), type = mapType)
-			map = openproj(x = map, projection = proj4string(x))
+			map = OpenStreetMap::openproj(x = map, projection = proj4string(x))
 		}
-		plot3d(x = coords[, 1], y = coords[, 2], z = time, xlab = xlab,
+		rgl::plot3d(x = coords[, 1], y = coords[, 2], z = time, xlab = xlab,
 			ylab = ylab, zlab = zlab, type = type, aspect = aspect, xlim = xlim,
 			ylim = ylim, zlim = zlim, ...)
-		if(showMap)
+		if (showMap)
 			map3d(map = map, z = time[1])
 	}
 )
@@ -306,8 +311,10 @@ setMethod("stcube", signature(x = "Tracks"),
 		ylim, zlim, showMap = FALSE, mapType = "osm", normalizeBy = "week", ..., y, z, col) {
 		# "y", "z" and "col" are ignored, but included in the method signature
 		# to avoid passing them twice to plot3d().
-		require(rgl)
-		require(OpenStreetMap)
+		if (!requireNamespace("rgl", quietly = TRUE))
+			stop("rgl required")
+		if (!requireNamespace("OpenStreetMap", quietly = TRUE))
+			stop("OpenStreetMap required")
 		dim = dim(x@tracks[[1]])["geometries"]
 		coordsAll = do.call(rbind, lapply(x@tracks, function(x) coordinates(x@sp)))
 		timeAll = normalize(do.call(c, lapply(x@tracks,
@@ -326,12 +333,13 @@ setMethod("stcube", signature(x = "Tracks"),
 		# rendering everything in one go.
 		if(showMap) {
 			# Required by openmap().
-			require(raster)
-			map = openmap(upperLeft = c(ylim[2], xlim[1]),
+			if (!requireNamespace("raster", quietly = TRUE))
+				stop("raster required")
+			map = OpenStreetMap::openmap(upperLeft = c(ylim[2], xlim[1]),
 				lowerRight = c(ylim[1], xlim[2]), type = mapType)
-			map = openproj(x = map, projection = proj4string(x))
+			map = OpenStreetMap::openproj(x = map, projection = proj4string(x))
 		}
-		plot3d(x = coordsAll[1:dim, 1], y = coordsAll[1:dim, 2],
+		rgl::plot3d(x = coordsAll[1:dim, 1], y = coordsAll[1:dim, 2],
 			z = timeAll[1:dim], xlab = xlab, ylab = ylab, zlab = zlab,
 			type = type, col = col[1], aspect = aspect, xlim = xlim,
 			ylim = ylim, zlim = zlim, ...)
@@ -339,7 +347,7 @@ setMethod("stcube", signature(x = "Tracks"),
 		for(t in seq_along(tracks)) {
 			coords = coordinates(tracks[[t]]@sp)
 			time = normalize(index(tracks[[t]]@time), normalizeBy, timeAll[1])
-			lines3d(x = coords[, 1], y = coords[, 2], z = time, col = col[t+1])
+			rgl::lines3d(x = coords[, 1], y = coords[, 2], z = time, col = col[t+1])
 		}
 		if(showMap)
 			map3d(map = map, z = timeAll[1])
@@ -351,8 +359,10 @@ setMethod("stcube", signature(x = "TracksCollection"),
 		ylim, zlim, showMap = FALSE, mapType = "osm", normalizeBy = "week", ..., y, z, col) {
 		# "y", "z" and "col" are ignored, but included in the method signature
 		# to avoid passing them twice to plot3d().
-		require(rgl)
-		require(OpenStreetMap)
+		if (!requireNamespace("rgl", quietly = TRUE))
+			stop("rgl required")
+		if (!requireNamespace("OpenStreetMap", quietly = TRUE))
+			stop("OpenStreetMap required")
 		dim = dim(x@tracksCollection[[1]]@tracks[[1]])["geometries"]
 		coordsAll = do.call(rbind, lapply(x@tracksCollection,
 			function(x) do.call(rbind, lapply(x@tracks, function(y) coordinates(y@sp)))))
@@ -373,12 +383,13 @@ setMethod("stcube", signature(x = "TracksCollection"),
 		# rendering everything in one go.
 		if(showMap) {
 			# Required by openmap().
-			require(raster)
-			map = openmap(upperLeft = c(ylim[2], xlim[1]),
+			if (!requireNamespace("raster", quietly = TRUE))
+				stop("raster required")
+			map = OpenStreetMap::openmap(upperLeft = c(ylim[2], xlim[1]),
 				lowerRight = c(ylim[1], xlim[2]), type = mapType)
-			map = openproj(x = map, projection = proj4string(x))
+			map = OpenStreetMap::openproj(x = map, projection = proj4string(x))
 		}
-		plot3d(x = coordsAll[1:dim, 1], y = coordsAll[1:dim, 2],
+		rgl::plot3d(x = coordsAll[1:dim, 1], y = coordsAll[1:dim, 2],
 			z = timeAll[1:dim], xlab = xlab, ylab = ylab, zlab = zlab,
 			type = type, col = col[1], aspect = aspect, xlim = xlim,
 			ylim = ylim, zlim = zlim, ...)
@@ -390,7 +401,7 @@ setMethod("stcube", signature(x = "TracksCollection"),
 			for(t in seq_along(tracks)) {
 				coords = coordinates(tracks[[t]]@sp)
 				time = normalize(index(tracks[[t]]@time), normalizeBy, timeAll[1])
-				lines3d(x = coords[, 1], y = coords[, 2], z = time, col = col[tz])
+				rgl::lines3d(x = coords[, 1], y = coords[, 2], z = time, col = col[tz])
 			}
 		}
 		if(showMap)
