948953ab5e1b6cace0d4fc4da58fecee59c875df
mpadge <mark.padgham@email.com>
2014-08-29 10:48:26 +0200

    explicit args for plot

--- R/Tracks-methods.R
+++ R/Tracks-methods.R
@@ -210,8 +210,15 @@ setMethod("plot", "TracksCollection",
 			ylim = stbox(x)[,2], col = 1, lwd = 1, lty =
 			1, axes = TRUE, Arrows = FALSE, Segments = FALSE, add = FALSE) {
 		sp = x@tracksCollection[[1]]@tracks[[1]]@sp
-		if (! add)
-			plot(as(sp, "Spatial"), xlim = xlim, ylim = ylim, axes = axes, ...)
+		if (! add) {
+                        a <- as.list (match.call ())
+                        a <- a [which (names (a) %in% 
+                                       names (c (par (), formals (plot.default))))]
+                        a <- a [3:length (a)] # [[1]]=the call, [[2]]=x
+                        a <- c (as (x, "Spatial"), a)
+                        do.call (plot, a)
+			#plot(as(sp, "Spatial"), xlim = xlim, ylim = ylim, axes = axes, ...)
+                }
 		if (axes == FALSE)
 			box()
 		if (Arrows || Segments) {
