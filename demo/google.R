9730d7b2b74c8cdf5246c77dfdd81eb0314e10c0
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2015-06-16 09:15:23 +0200

    add google locationhistory example

---
+++ demo/google.R
@@ -0,0 +1,23 @@
+# read a file obtained by "Export to kml" from https://maps.google.com/locationhistory/
+require(trajectories)
+filename = system.file("history-06-09-2015.kml", package="trajectories")
+
+library(XML)
+kml <- xmlToList(filename)
+
+tr = kml$Document$Placemark$Track
+cc = which(names(tr) == "coord")
+coord = t(sapply(kml$Document$Placemark$Track[cc], function(x) scan(text = x, quiet = TRUE)))[,1:2]
+when = which(names(tr) == "when")
+# convert the "-07:00" into " -0700" with sub:
+time = strptime(sub("-07:00$", " -0700", unlist(kml$Document$Placemark$Track[when])),
+	"%Y-%m-%dT%H:%M:%OS %z")
+
+library(sp)
+library(spacetime)
+library(trajectories)
+track = Track(STI(SpatialPoints(coord, CRS("+proj=longlat +ellps=WGS84")), 
+	time))
+summary(track)
+as(track, "data.frame")
+plot(track, axes = TRUE)
