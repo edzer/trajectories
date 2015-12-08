ea01fa9cd300ff860959d5f82a107ea4c3d3cb7b
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2015-12-08 15:40:53 +0100

    add sd1/2/3, improve examples

--- R/rtracks.R
+++ R/rtracks.R
@@ -1,18 +1,19 @@
 rTrack = function(n = 100, origin = c(0,0), start = as.POSIXct("1970-01-01"), ar = .8, 
-		step = 60, sd = 1, ...) { 
+		step = 60, sd0 = 1, ...) { 
 	if (length(ar) == 1 && ar == 0)
-		xy = cbind(cumsum(rnorm(n, sd = sd)) + origin[1], cumsum(rnorm(n, sd = sd)) + origin[2])
+		xy = cbind(cumsum(rnorm(n, sd = sd0)) + origin[1], cumsum(rnorm(n, sd = sd0)) + origin[2])
 	else 
-		xy = cbind(origin[1] + cumsum(as.vector(arima.sim(list(ar = ar), n, sd = sd, ...))),
-			       origin[2] + cumsum(as.vector(arima.sim(list(ar = ar), n, sd = sd, ...))))
+		xy = cbind(origin[1] + cumsum(as.vector(arima.sim(list(ar = ar), n, sd = sd0, ...))),
+			       origin[2] + cumsum(as.vector(arima.sim(list(ar = ar), n, sd = sd0, ...))))
 	T = start + 0:(n-1) * step
 	sti = STI(SpatialPoints(xy), T)
 	Track(sti)
 }
 
-rTracks = function(m = 20, start = as.POSIXct("1970-01-01"), delta = 7200, sdo = 0, ...)
+rTracks = function(m = 20, start = as.POSIXct("1970-01-01"), delta = 7200, sd1 = 0, 
+		origin = c(0,0), ...)
 	Tracks(lapply(0:(m-1) * delta, function(x) rTrack(start = start + x, 
-		origin = rnorm(2, sd = sdo), ...)))
+		origin = origin + rnorm(2, sd = sd1), ...)))
 
-rTracksCollection = function(p = 10, ...)
-	TracksCollection(lapply(1:p, function(x) rTracks(...)))
+rTracksCollection = function(p = 10, sd2 = 0, ...)
+	TracksCollection(lapply(1:p, function(x) rTracks(origin = rnorm(2, sd = sd2) ,...)))
