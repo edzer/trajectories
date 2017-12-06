5e70c8cacf4daadb1b04b2f3099aba6691786f32
Moradii <m2.moradi@yahoo.com>
2017-12-06 15:29:45 +0100

    newrTrack,newrTracks,newrTracksCollection
    these three functions accept box for simulating tracks

--- R/Trackstat.R
+++ R/Trackstat.R
@@ -360,3 +360,55 @@ plot.gTrack <- function(x,type="l",col= "grey70",...){
   points(x$r,x$theo,type=type,col=2)
   points(x$r,x$aveg,type=type)
 }
+
+
+newrTrack <- function (n = 100, origin = c(0, 0), start = as.POSIXct("1970-01-01"), 
+                       ar = 0.8, step = 60, sd0 = 1,bbox=bbox, transform=FALSE, ...){
+  if (missing(bbox) & transform) {
+    xo <- runif(1)
+    yo <- runif(1)
+    origin <- c(xo,yo)
+  }  
+  if (!missing(bbox) & transform) {
+    xo <- runif(1,bbox[1,1],bbox[1,2])
+    yo <- runif(1,bbox[2,1],bbox[2,2])
+    origin <- c(xo,yo)
+  }
+  if (length(ar) == 1 && ar == 0) 
+    xy = cbind(cumsum(rnorm(n, sd = sd0)) + origin[1], cumsum(rnorm(n, 
+                                                                    sd = sd0)) + origin[2])
+  else {xy = cbind(origin[1] + cumsum(as.vector(arima.sim(list(ar = ar), 
+                                                          n, sd = sd0, ...))), 
+                   origin[2] + cumsum(as.vector(arima.sim(list(ar = ar), 
+                                                          
+                                                          n, sd = sd0, ...))))}
+  if(transform) {
+    if(missing(bbox))  bbox <- matrix(c(0,1,0,1),nrow = 2,byrow = T); colnames(bbox) <- c("min","max");rownames(bbox) <- c("x","y")
+    
+    xr <- max(xy[,1])-min(xy[,1])
+    yr <- max(xy[,2])-min(xy[,2])
+    
+    xt <- (xy[,1]-min(xy[,1]))/xr
+    yt <- (xy[,2]-min(xy[,2]))/yr
+    
+    xy <- cbind(xt,yt)
+    xy <- cbind(x=xy[,1]*bbox[1,2],y=xy[,2]*bbox[2,2])
+  }
+  
+  T = start + 0:(n - 1) * step
+  sti = STI(SpatialPoints(xy), T)
+  out <- Track(sti)
+  if (transform) out@sp@bbox <- bbox
+  return(out)
+}
+
+
+newrTracks <- function (m = 20, start = as.POSIXct("1970-01-01"), delta = 7200, 
+                        sd1 = 0, origin = c(0, 0), ...) 
+  Tracks(lapply(0:(m - 1) * delta, function(x) newrTrack(start = start + 
+                                                    x, origin = origin + rnorm(2, sd = sd1), ...)))
+
+newrTracksCollection <- function (p = 10, sd2 = 0, ...) 
+  TracksCollection(lapply(1:p, function(x) newrTracks(origin = rnorm(2, 
+                                                              sd = sd2), ...)))
+
