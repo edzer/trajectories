2d35f0ad84880e79eadd479b793b00f37aa11bdf
Moradii <m2.moradi@yahoo.com>
2017-12-11 15:48:49 +0100

    auto.arima.Track is added
    it enables users to fit an Arima model to an object of class “Track”.

--- R/Trackstat.R
+++ R/Trackstat.R
@@ -415,3 +415,26 @@ rTracksCollection <- function (p = 10, sd2 = 0, ...)
   TracksCollection(lapply(1:p, function(x) rTracks(origin = rnorm(2, 
                                                                   sd = sd2), ...)))
 
+
+auto.arima.Track <- function(X,...){
+  stopifnot(class(X)=="Track")
+  xseries <- coordinates(X)[,"x"]
+  yseries <- coordinates(X)[,"y"]
+  
+  xfit <- auto.arima(xseries,...)
+  yfit <- auto.arima(yseries,...)
+  
+  out <- list(xfit,yfit)
+  attr(out,"models") <- out
+  class(out) <- c("ArimaTrack")
+  return(out)
+}
+
+print.ArimaTrack <- function(X){
+  attributes(X) <- NULL
+  cat("Arima model fitted to x-coordinate: ");
+  cat(paste0(X[[1]]),"\n")
+  cat("Arima model fitted to y-coordinate: ");
+  cat(paste0(X[[2]]))
+}
+
